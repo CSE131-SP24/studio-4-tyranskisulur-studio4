06f72cad7f1c9d2e3aab6fadbfa7662f01e6fec8
larat <larat@laralaptop-_-.dhcp.wustl.edu>
2024-02-22 12:41:12 -0600

    Studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -2,10 +2,14 @@ package studio4;
 
 import java.awt.Color;
 
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.picture(0.5, 0.5, "m&m.jpg", 0.5, 0.5);
+		StdDraw.setPenRadius(0.1);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.ellipse(0.5, 0.5, 0.3, 0.3);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,7 @@
 package studio4;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -8,6 +9,8 @@ import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
+
+import java.awt.Color;
 
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
@@ -20,5 +23,51 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next(); 
+		
+		int red;
+		int green;
+		int blue;
+		boolean filled;
+		double point1;
+		double point2;
+		double point3;
+		double point4;
+		
+		if(shapeType.equals("rectangle")) {
+			red = in.nextInt(); 
+			green = in.nextInt(); 
+			blue = in.nextInt(); 
+			filled = in.nextBoolean(); 
+			point1 = in.nextDouble();
+			point2 = in.nextDouble();
+			point3 = in.nextDouble();
+			point4 = in.nextDouble();
+			Color ooga = new Color(red, green, blue);
+			StdDraw.setPenColor(ooga);
+			if(filled == true) {
+				StdDraw.filledRectangle(point1, point2, point3, point4);
+			}else {
+				StdDraw.rectangle(point1, point2, point3, point4);
+			}
+			StdDraw.rectangle(point1, point2, point3, point4);
+		}else if (shapeType.equals("ellipse")){
+			red = in.nextInt(); 
+			green = in.nextInt(); 
+			blue = in.nextInt(); 
+			filled = in.nextBoolean(); 
+			point1 = in.nextDouble();
+			point2 = in.nextDouble();
+			point3 = in.nextDouble();
+			point4 = in.nextDouble();
+			Color ooga = new Color(red, green, blue);
+			StdDraw.setPenColor(ooga);
+			if(filled == true) {
+				StdDraw.filledEllipse(point1, point2, point3, point4);
+			}else {
+				StdDraw.ellipse(point1, point2, point3, point4);
+			}
+		}
+				
 	}
 }
